2c50a7f9b8d4b41cb0b5fb58143501765e36688d
Johann Gomez <johanngomez09@gmail.com>
2022-12-08 16:57:11 -0300

    api mejoras

---
+++ src/helpers/mejoras.js
@@ -0,0 +1,16 @@
+import { axiosApi } from "./api";
+
+export const getMejoras = async() => {
+
+    const token = localStorage.getItem('token');
+
+    const mejorasResp = axiosApi.get('/mejoras', {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    })
+
+    const { data } = mejorasResp;
+
+    return data;   
+}
