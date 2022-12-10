7c25c32e78e08ed0490637bc728b934a69c26f51
Johann Gomez <johanngomez09@gmail.com>
2022-12-10 15:49:48 -0300

    update

--- src/helpers/mejoras.js
+++ src/helpers/mejoras.js
@@ -3,7 +3,7 @@ import { axiosApi } from "./api";
 export const getMejoras = async() => {
 
     const token = localStorage.getItem('token');
-
+// await
     const mejorasResp = await axiosApi.get('/mejoras', {
         headers: {
             'Authorization': 'Bearer ' + token
