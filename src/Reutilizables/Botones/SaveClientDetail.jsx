b56996705aacf58e0af19f5fe32e844404064165
Johann Gomez <johanngomez09@gmail.com>
2022-12-10 13:58:42 -0300

    update

--- src/Reutilizables/Botones/SaveClientDetail.jsx
+++ src/Reutilizables/Botones/SaveClientDetail.jsx
@@ -3,6 +3,7 @@ import ToggleButton from 'react-bootstrap/ToggleButton';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import { Link } from "react-router-dom";
 import { DetalleCliente } from '../Modals/DetalleCliente';
+import { EliminarCliente } from '../Modals/EliminarCliente';
 
 export const SaveClientDetail = () => {
 
@@ -11,6 +12,14 @@ export const SaveClientDetail = () => {
   const showDetailClient = () => {
     // setTimeout( () => {
     setStateDetailClient(true);
+    // }, 5000 );  
+  }
+
+  const [stateEraseClient, setStateEraseClient] = useState(false);
+
+  const showEraseClient = () => {
+    // setTimeout( () => {
+    setStateEraseClient(true);
     // }, 5000 );  
   }
 
@@ -23,7 +32,7 @@ export const SaveClientDetail = () => {
             <ToggleButton onClick={showDetailClient} id="tbg-check-1" variant="primary" value={1}>
               Guardar cambios
             </ToggleButton>
-            <ToggleButton id="tbg-check-2" variant="danger" value={2}>
+            <ToggleButton onClick={showEraseClient} id="tbg-check-2" variant="danger" value={2}>
               Eliminar cliente
             </ToggleButton>
             <ToggleButton id="tbg-check-2" variant="success" value={3}>
@@ -35,6 +44,10 @@ export const SaveClientDetail = () => {
 
       {
         (stateDetailClient) ? <DetalleCliente setStateDetailClient={setStateDetailClient} /> : ""
+      }
+
+      {
+        (stateEraseClient) ? <EliminarCliente setStateEraseClient={setStateEraseClient} /> : ""
       }
     </div>
   )
